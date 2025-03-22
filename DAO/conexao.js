627f062d5d55ff78f688cf47b1c18819ff4c6483
LaralizPaivaDev <lelulamagui@gmail.com>
2025-03-22 20:04:50 -0300

    new file:   .gitignore 	new file:   DAO/conexao.js 	new file:   DAO/produto.js 	new file:   DAO/venda.js 	new file:   index.js 	new file:   package-lock.json 	new file:   package.json 	new file:   public/css/editar_produto.css 	new file:   public/css/formulario_produto.css 	new file:   public/css/formulario_venda.css 	new file:   public/css/inicial.css 	new file:   public/css/listagem_produto.css 	new file:   public/css/listagem_venda.css 	new file:   public/image/logo_carla_irmaos_games_fundo_azul.png 	new file:   public/image/logo_carla_irmaos_games_fundo_branco.png 	new file:   vercel.json 	new file:   views/editarProduto.handlebars 	new file:   views/formularioProduto.handlebars 	new file:   views/formularioVenda.handlebars 	new file:   views/inicial.handlebars 	new file:   views/layouts/main.handlebars 	new file:   views/listagemProduto.handlebars 	new file:   views/listagemVenda.handlebars

---
+++ DAO/conexao.js
@@ -0,0 +1,41 @@
+// Importando modulos
+const mysql2 = require('mysql2');
+const Sequelize = require('sequelize');
+
+// Configuração da conexão com o banco de dados MySQL
+//const sequelize = new Sequelize('railway', 'root', 'oAOJnWCesZqqRJkmdUaSWCyYEGBfYZzJ', {
+    // timestamps - retirada da criacao automatica das colunas createAt e updatedAt
+    //define: {
+    //    timestamps: false,
+    //},
+    //host: 'viaduct.proxy.rlwy.net',
+    //dialect: 'mysql',
+    //dialectModule: mysql2,
+    //port: '45429'
+//})
+
+// Configuração da conexão com o banco de dados MySQL
+const sequelize = new Sequelize('sistema_estokey', 'root', '', {
+    // timestamps - retirada da criacao automatica das colunas createAt e updatedAt
+    define: {
+        timestamps: false,
+    },
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    port: '3306'
+
+})
+
+// Verificacao da conexao com o banco de dados
+sequelize.authenticate().then(() => {
+    console.log('Conexão bem sucedida ao banco de dados MySQL');
+}).catch((erro) => {
+    console.error('Erro ao conectar ao banco de dados: ', erro);
+})
+
+// Exportando modulos
+module.exports = {
+    Sequelize : Sequelize,
+    sequelize : sequelize
+}
---
+++ DAO/venda.js
@@ -0,0 +1,56 @@
+// Importando modulos
+const { FOREIGNKEYS } = require('sequelize/lib/query-types');
+const conexao = require('./conexao');
+const produto = require('./produto')
+
+//----------------------- Tela de Venda ----------------------------- 
+const Venda = conexao.sequelize.define("tbl_venda", {
+    pk_venda: {
+        type: conexao.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    data_venda: {
+        type: conexao.Sequelize.DATE,
+        allowNull: false
+    },
+    fk_produto: {
+        type: conexao.Sequelize.INTEGER,
+        allowNull: false
+    },
+    fk_valor_venda_produto: {
+        type: conexao.Sequelize.INTEGER,
+        allowNull: false
+    },
+    quantidade_venda_produto: {
+        type: conexao.Sequelize.INTEGER,
+        allowNull: false
+    },
+    total_pedido_venda: {
+        type: conexao.Sequelize.DOUBLE,
+        allowNull: false
+    },
+    desconto_venda: {
+        type: conexao.Sequelize.DOUBLE,
+        allowNull: false
+    },
+    total_pago_venda: {
+        type: conexao.Sequelize.DOUBLE,
+        allowNull: false
+    },
+    forma_pag_venda: {
+        type: conexao.Sequelize.STRING,
+        allowNull: false
+    }
+    
+}, { freezeTableName: true });
+
+// Definir relacionamento (Uma Venda tem um Produto)
+Venda.belongsTo(produto, {
+    constraint: true,
+    foreignKey: "fk_produto",
+    as: "tbl_produto",
+});
+
+module.exports = Venda
