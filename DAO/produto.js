63c92aa15f7a02bd2c9b59882c676c23cb8de25f
LaralizPaivaDev <lelulamagui@gmail.com>
2025-03-23 12:24:33 -0300

    modified:   DAO/produto.js 	modified:   DAO/venda.js

--- DAO/produto.js
+++ DAO/produto.js
@@ -1,42 +1,41 @@
 // Importando modulos
-const { FOREIGNKEYS } = require('sequelize/lib/query-types');
-const conexao = require('./conexao');
+const { FOREIGNKEYS } = require("sequelize/lib/query-types");
+const conexao = require("./conexao");
 
 //----------------------- Tela de Produto ---------------------------
-const Produto = conexao.sequelize.define("tbl_produto", {
+const Produto = conexao.sequelize.define(
+  "tbl_produto",
+  {
     pk_produto: {
-        type: conexao.Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
+      type: conexao.Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
     descricao_produto: {
-        type: conexao.Sequelize.STRING,
-        allowNull: false
+      type: conexao.Sequelize.STRING,
+      allowNull: false,
     },
     categoria_produto: {
-        type: conexao.Sequelize.STRING,
-        allowNull: false
+      type: conexao.Sequelize.STRING,
+      allowNull: false,
     },
     valor_compra_produto: {
-        type: conexao.Sequelize.DOUBLE,
-        allowNull: false
+      type: conexao.Sequelize.DOUBLE,
+      allowNull: false,
     },
     valor_bruto_venda_produto: {
-        type: conexao.Sequelize.DOUBLE,
-        allowNull: false
+      type: conexao.Sequelize.DOUBLE,
+      allowNull: false,
     },
     quantidade_produto: {
-        type: conexao.Sequelize.INTEGER,
-        allowNull: false
-    }
-    
-}, { freezeTableName: true });
+      type: conexao.Sequelize.INTEGER,
+      allowNull: false,
+    },
+  },
+  { freezeTableName: true }
+);
 
-//Post.sync({force: true});
+Post.sync({ force: true });
 
 module.exports = Produto;
-
-
-
-
--- DAO/venda.js
+++ DAO/venda.js
@@ -1,56 +1,61 @@
 // Importando modulos
-const { FOREIGNKEYS } = require('sequelize/lib/query-types');
-const conexao = require('./conexao');
-const produto = require('./produto')
+const { FOREIGNKEYS } = require("sequelize/lib/query-types");
+const conexao = require("./conexao");
+const produto = require("./produto");
 
-//----------------------- Tela de Venda ----------------------------- 
-const Venda = conexao.sequelize.define("tbl_venda", {
+//----------------------- Tela de Venda -----------------------------
+const Venda = conexao.sequelize.define(
+  "tbl_venda",
+  {
     pk_venda: {
-        type: conexao.Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
+      type: conexao.Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
     data_venda: {
-        type: conexao.Sequelize.DATE,
-        allowNull: false
+      type: conexao.Sequelize.DATE,
+      allowNull: false,
     },
     fk_produto: {
-        type: conexao.Sequelize.INTEGER,
-        allowNull: false
+      type: conexao.Sequelize.INTEGER,
+      allowNull: false,
     },
     fk_valor_venda_produto: {
-        type: conexao.Sequelize.INTEGER,
-        allowNull: false
+      type: conexao.Sequelize.INTEGER,
+      allowNull: false,
     },
     quantidade_venda_produto: {
-        type: conexao.Sequelize.INTEGER,
-        allowNull: false
+      type: conexao.Sequelize.INTEGER,
+      allowNull: false,
     },
     total_pedido_venda: {
-        type: conexao.Sequelize.DOUBLE,
-        allowNull: false
+      type: conexao.Sequelize.DOUBLE,
+      allowNull: false,
     },
     desconto_venda: {
-        type: conexao.Sequelize.DOUBLE,
-        allowNull: false
+      type: conexao.Sequelize.DOUBLE,
+      allowNull: false,
     },
     total_pago_venda: {
-        type: conexao.Sequelize.DOUBLE,
-        allowNull: false
+      type: conexao.Sequelize.DOUBLE,
+      allowNull: false,
     },
     forma_pag_venda: {
-        type: conexao.Sequelize.STRING,
-        allowNull: false
-    }
-    
-}, { freezeTableName: true });
+      type: conexao.Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  { freezeTableName: true }
+);
 
 // Definir relacionamento (Uma Venda tem um Produto)
 Venda.belongsTo(produto, {
-    constraint: true,
-    foreignKey: "fk_produto",
-    as: "tbl_produto",
+  constraint: true,
+  foreignKey: "fk_produto",
+  as: "tbl_produto",
 });
 
-module.exports = Venda
+Post.sync({ force: true });
+
+module.exports = Venda;
